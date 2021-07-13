48c78e12b086b125aee344d01d2625a3085a7a42
AldoVidales <aldo.vidales@gmail.com>
2021-07-13 00:04:23 -0500

    31

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from ventana import ventana3
 
 t=ventana3()
 t.cargar()
+
--- ventana.py
+++ ventana.py
@@ -1,5 +1,7 @@
 
 from tkinter import *
+from tkinter import ttk
+
 
 class ventana3:
     def __init__(self):
@@ -7,6 +9,19 @@ class ventana3:
         self.size="770x470"
         self.resiable=False
         self.icon='C:/Users/aldov/Documents/Programacion/Python/Curso tkinter/imagenes\internet-speed.ico'
+        
+        
+    
+    def suma(self):
+
+     try: 
+        self.suma= int(self.E1.get())*(1/1000)*(3600/1)
+        return self.var.set(self.suma)
+
+     except:
+         print("No se pudo sorry") 
+
+        
 
 
   
@@ -25,12 +40,14 @@ class ventana3:
         self.ventana.geometry(self.size)
         self.ventana.resizable(0,0)
         self.ventana.iconbitmap(self.icon)
+        self.var=StringVar()
         titulo=Label(self.ventana,text="Convertidor")
         titulo.grid(column=1,row=1)
         co2=Label(self.ventana, text="km/h")
         co2.grid(column=1,row=2)
         co=Label(self.ventana, text="m/s")
         co.grid(column=6,row=5)
+        
 
         self.E1 = Entry(self.ventana)
         self.E1.grid(column=1,row=3)
@@ -39,30 +56,26 @@ class ventana3:
         # oky=Button(self.ventana,text="OK",command=self.convertir)
         # oky.pack()
 
-        oky=Button(self.ventana,text="OK",command=self.convertir)
+        oky=Button(self.ventana,text="OK",command=self.suma)
         oky.grid(column=6,row=3)
+
+
+        self.eq=Label(self.ventana,textvariable=self.var)
+           
+        self.eq.grid(column=6,row=6)
 
        
     
         self.ventana.mainloop()
+
+
+        
         
 
-    def convertir(self):
-        
+   
+            
 
-        if self.E1.get()==type(str):
-            print('no se aceptan letras')
 
-        else:
-            
-            
-    
-            ms=float(self.E1.get())*(1/1000)*(3600/1)
-            self.eq=Label(self.ventana,text=ms)
-            self.eq.grid(column=6,row=6)
-            
-            
-            print(ms)
            
 
 
